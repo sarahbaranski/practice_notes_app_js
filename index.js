350ed8ad2b933331646ee9ae4b81422bbfaf5bf3
sarahbaranski <sbaranski@gmail.com>
2022-01-22 10:59:50 -0600

    added methods to sort and format notes

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
+function transformNotes() {
+  return notes.slice().sort(function (a, b) {
+    return b.timestamp - a.timestamp;
+  });
+}
+
+function formatTimestamp(timestamp) {
+  return new Date(timestamp).toUTCString();
+}
+
+function formatTitle(body) {
+  var maxLength = 20;
+  if (body.length > maxLength) {
+    return body.substring(0, maxLength - 3) + "...";
+  } else if (body.length === 0) {
+    return "New note";
+  } else {
+    return body;
+  }
+}
+
 var notes = [
-  { id: 1, body: "This is a first test", timestamp: Date.now() },
-  { id: 2, body: "This is a second test", timestamp: Date.now() },
-  { id: 3, body: "This is a third test", timestamp: Date.now() },
+  { id: 1, body: "This is a first test", timestamp: Date.now() - 300000000 },
+  { id: 2, body: "This is a second test", timestamp: Date.now() - 200000000 },
+  { id: 3, body: "", timestamp: Date.now() - 100000000 },
   { id: 4, body: "This is a fourth test", timestamp: Date.now() },
 ];
 
 var htmlString = "";
-notes.forEach(function (note) {
+transformNotes(notes).forEach(function (note) {
   htmlString += `
     <div class="note-selector">
-    <p class="note-selector-title">${note.body}</p>
-    <p class="note-selector-timestamp">${note.timestamp}</p>
+    <p class="note-selector-title">${formatTitle(note.body)}</p>
+    <p class="note-selector-timestamp">${formatTimestamp(note.timestamp)}</p>
     </div>
   `;
 });
