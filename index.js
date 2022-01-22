996c56e5f5c2aa02b12ffeb4d0f4ef2e15483e55
sarahbaranski <sbaranski@gmail.com>
2022-01-22 10:38:16 -0600

    added js defer to load js after html, and addeed loop for note selector

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Hello from JavaScript!");
+var notes = [
+  { id: 1, body: "This is a first test", timestamp: Date.now() },
+  { id: 2, body: "This is a second test", timestamp: Date.now() },
+  { id: 3, body: "This is a third test", timestamp: Date.now() },
+  { id: 4, body: "This is a fourth test", timestamp: Date.now() },
+];
+
+var htmlString = "";
+notes.forEach(function (note) {
+  htmlString += `
+    <div class="note-selector">
+    <p class="note-selector-title">${note.body}</p>
+    <p class="note-selector-timestamp">${note.timestamp}</p>
+    </div>
+  `;
+});
+document.querySelector(".note-selectors").innerHTML = htmlString;
